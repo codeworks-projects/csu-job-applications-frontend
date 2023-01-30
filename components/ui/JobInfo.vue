c3cf5e476b6d91a7f677450ddf27e8da58298a3f
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2023-01-30 21:46:45 +0100

    feat: toggleShow method incomplete

--- components/ui/JobInfo.vue
+++ components/ui/JobInfo.vue
@@ -26,20 +26,31 @@
             v-if="isSingleJob === true"
             class="section-wrapper"
         >
-        
-            <div class="section">
+            <div class="section"
+                :class="{
+                    'orange': isSingleJob,
+                }"
+            >
                 <h1>Jobbone</h1>
-                <ul class="info-job">
-                    <li>Luogo</li>
-                    <li>Full-time</li>
-                </ul>
+                <!-- v-if="isShown includes id" -->
+                <p class="info-job">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, suscipit.
+                </p>
+                <div class="toggle">
+                    <div
+                        class="sign"
+                        @click="toggleShow()"
+                    >
+                        <span v-if="isShowned.includes(0)">&plus;</span>
+                        <span v-else>&minus;</span>
+                    </div>
+                </div>
             </div>
-                <div class="section">
+            <div class="section">
                 <h1>Job Due</h1>
-                <ul class="info-job">
-                    <li>Luogo</li>
-                    <li>Tempo</li>
-                </ul>
+                <p class="info-job">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, suscipit. Lorem ipsum dolor sit amet consectetur.
+                </p>
             </div>
         </div>
     </div>
@@ -56,6 +67,26 @@ export default {
             type: Boolean,
             default: false
         },
+    },
+    data() {
+        return {
+            // Se ci metto un array che si riempie e si svuota in base a cosa clicco allora ottengo l'apertura di tutte le cose che voglio per confronto diretto fra id cliccato e array riempito
+
+            isShowned: [],
+        }
+    },
+    computed: {
+
+    },
+    method: {
+        toggleShow(id){
+            if (!this.isShowned.includes(id)){
+                this.isShowned.push(id)
+            } else {
+                const index = this.isShowned.indexOf(id);
+                this.isShowned.splice(index, 1);
+            }
+        }
     }
 
 }
@@ -63,32 +94,51 @@ export default {
 
 <style lang="postcss" scoped>
 .section-ct {
-    & .section {
-        @apply w-full mt-6 pt-2 pb-4 border-t-2 border-t-primary text-grey;
-            
-        & h1 {
-            @apply text-4xl pt-4 font-light;
-        }
+    & .section-wrapper {
+        & .section {
+            @apply relative w-full mt-6 pt-2 pb-4 border-t-2 border-t-primary text-grey;
+                
+            & h1 {
+                @apply text-4xl pt-4 font-light;
+            }
+            & .info-job {
+                @apply mt-8 flex;
 
-        & .info-job {
-            @apply mt-8 flex;
+                & li {
+                    @apply mx-1;
 
-            & li {
-                @apply mx-1;
+                    &::after {
+                        content: ',';
+                    }
 
-                &::after {
-                    content: ',';
+                    &:last-of-type::after {
+                        content: '.';
+                    }   
+                }
+            }
+            & .toggle {
+                @apply flex justify-center items-center absolute right-0 top-1/2 border-2 border-primary h-10 w-10;
+
+                border-radius: 50%;
+
+                &:hover {
+                    @apply cursor-pointer;
                 }
 
-                &:last-of-type::after {
-                    content: '.';
-                }   
+                & .sign {
+                    @apply text-primary text-xl;
+                }
             }
         }
-    }
-    & .section:first-child {
-        & h1 {
-            @apply text-black;
+        & .section:first-child {
+            & h1 {
+                @apply text-black;
+            }
+        }
+        & .section.orange:first-child {
+            & h1 {
+                @apply text-primary;
+            }
         }
     }
 }
--- pages/jobs/index.vue
+++ pages/jobs/index.vue
@@ -1,20 +1,27 @@
 <template>
-    <div class="section-ct">
-        <h1></h1>
+    <div class="page">
         <main>
-            <JobInfo
-                :is-single-job="true"
-            />
+            <div class="title">
+                <h1>Job Title</h1>
+            </div>
+            <JobInfo :is-single-job="true" />
         </main>
     </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="postcss" scoped>
+.page {
+    @apply text-base flex-col justify-center;
 
+    & main {
+        @apply mt-28 p-4;
+        & .title {
+            @apply mt-44 mb-28 text-6xl;
+        }
+    }
+}
 </style>
