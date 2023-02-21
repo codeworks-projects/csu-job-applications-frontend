d61e64f69b6886b94b725e261683231be2ec8294
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2023-02-21 19:06:00 +0100

    style(footer): new footer style with links

--- components/navigation/Footer.vue
+++ components/navigation/Footer.vue
@@ -1,36 +1,49 @@
 <template>
   <footer>
     <div class="container center">
-      <div class="box-left">
-        <ul>
-          <li>{{ $t("footer.didYouNotFindThePositionYouWereLookingFor") }}</li>
-          <li>{{ $t("footer.weAreAlwaysHereForNewCadidates") }}</li>
-        </ul>
-        <a href="mailto:info@csu-online.it" class="btn-ct">
-          <Button
-            :value="$t('common.info') + '@' + $t('common.csuEmail')"
-            type="secondary"
-            icon="arrow-up-right"
-          />
-        </a>
+      <div class="title">
+        <p>{{ $t('footer.didYouNotFindThePositionYouWereLookingFor') }}</p>
+        <p>{{ $t('footer.weAreAlwaysHereForNewCadidates') }}</p>
       </div>
-      <div class="box-right">
-        <ul class="coop-ct">
-          <li>{{ $t("footer.csuCoop") }}</li>
-          <li>{{ $t("common.csuAddress") }}</li>
-        </ul>
-        <ul>
-          <li>{{ $t("common.contacts") }}</li>
-          <li>
-            {{ $t("common.csuTelephone") }}
-            <br />
-            {{ $t("common.csuEmail") }}
-          </li>
-          <li>{{ $t("common.pIva") }}</li>
-          <li>{{ $t("common.actualIva") }}</li>
-          <li>{{ $t("footer.destinationCode") }}</li>
-          <li>{{ $t("footer.actualDestinationCode") }}</li>
-        </ul>
+    
+      <div class="grid-ct">
+        <div class="address-ct">
+          <p class="font-semibold">
+            {{ $t('footer.csu') }} 
+            &verbar; 
+            {{ $t('footer.coop') }}
+          </p>
+          <p>{{ $t('common.csuAddress') }}</p>
+          <p>{{ $t('common.capAndBz') }}</p>
+        </div>
+        <div class="contacts-ct">
+          <p class="font-semibold">{{ $t('common.contacts') }}</p>
+          <p class="underline">
+            <a href="tel:+390471324239">{{ $t('common.csuTelephone') }}</a>
+            </p>
+          <p class="underline">
+            <a href="mailto:info@csu-online.it">{{ $t('common.info') }}&commat;{{ $t('common.csuEmail') }}</a>
+          </p>
+        </div>
+        <div class="iva-ct">
+          <p>{{ $t('common.pIva') }}</p>
+          <p>{{ $t('common.destinationCode') }}</p>
+        </div>
+        <div class="links-ct">
+          <a href="https://irp.cdn-website.com/78321997/files/uploaded/risk%20map%20CSU%20rev.%2012%202022.pdf">{{ $t('footer.riskMap') }}</a>
+          <a href="https://www.csu-online.it/codice-etico">{{ $t('footer.ethicCode') }}</a>
+          <a href="https://www.csu-online.it/risk-map">{{ $t('footer.mog') }}</a>
+        </div>
+      </div>
+
+      <div class="copyright">
+        &copy;
+        {{ $t('footer.allRightsReserved') }}
+        &verbar;
+        {{ $t('footer.csu') }}
+        {{ $t('footer.coop') }}
+        &CenterDot;
+        <a href="https://www.csu-online.it/privacy-e-cookie">{{ $t('footer.privacyAndCoockie') }}</a>
       </div>
     </div>
   </footer>
@@ -42,38 +55,32 @@ export default {};
 
 <style lang="postcss" scoped>
 footer {
-  @apply bg-primary text-sm text-white py-10;
+  @apply bg-primary text-sm text-white pt-14 pb-4;
 
-  & .container {
-    @apply grid grid-cols-2;
-    & .box-left {
-      @apply w-1/2 flex flex-col justify-start;
-
-      & ul {
-        & li {
-          @apply even:w-2/3 
-                        odd:w-1/2 odd:mb-2 odd:text-light-grey odd:opacity-50;
-        }
+    & .container {
+      & .title {
+        @apply text-center text-2xl mb-14;
       }
-
-      & .btn-ct {
-        @apply flex;
+      & .grid-ct {
+        @apply grid grid-cols-5;
+          & .iva-ct{
+            @apply grid col-start-4 col-end-4;
+          }
+          & .links-ct {
+            @apply grid col-start-5 col-end-5 font-semibold underline;
+            & a {
+              @apply block;
+            }
+          }
       }
-    }
-    & .box-right {
-      @apply flex;
-      & .coop-ct {
-        @apply flex flex-col;
-      }
+      & .copyright {
+        @apply text-center mt-14;
 
-      & ul {
-        @apply w-1/2 h-1/2;
-        & li {
-          @apply odd:mt-4 odd:text-light-grey odd:uppercase odd:opacity-50;
+        & a {
+          @apply underline;
         }
       }
     }
-  }
 }
 
 @media only screen and (max-width: 980px) {
@@ -81,14 +88,20 @@ footer {
     @apply py-12;
 
     & .container {
-      @apply flex flex-col gap-6;
-
-      & .box-left {
-        @apply flex-col;
+      & .title {
+        @apply text-xl;
       }
 
-      & .box-right {
-        @apply flex-col;
+      & .grid-ct {
+        @apply flex flex-col items-center text-center gap-6;
+
+        & .box-left {
+          @apply flex-col;
+        }
+
+        & .box-right {
+          @apply flex-col;
+        }
       }
     }
   }
@@ -97,8 +110,10 @@ footer {
 @media only screen and (max-width: 500px) {
   footer {
     & .container {
-      & .box-left {
-        @apply w-full;
+      & .grid-ct {
+        & .box-left {
+          @apply w-full;
+        }
       }
     }
   }
