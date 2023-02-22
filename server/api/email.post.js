873249580733f438528c0146ae1e3e7be2c417db
Alessio Crea <alessio.crea98@gmail.com>
2023-02-22 11:05:15 +0100

    feat: update env example

--- server/api/email.post.js
+++ server/api/email.post.js
@@ -3,7 +3,6 @@ import { readBody } from "h3";
 import sendContactEmail from "~/server/utilities/sendgrid.js";
 
 export default defineEventHandler(async (event) => {
-  //   console.log(req, res);
   const body = await readBody(event);
   const emailSent = await sendContactEmail(body);
 
