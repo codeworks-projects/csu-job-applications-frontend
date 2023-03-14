24582d1197438dd3f6fe6f99fa66736e6ca6aebe
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2023-03-14 12:02:05 +0100

    feat: add de translation

--- server/utilities/sendgrid.js
+++ server/utilities/sendgrid.js
@@ -9,7 +9,7 @@ export default function sendContactEmail(fields) {
     return { error: "Required field(s) missing", success: false };
   }
   const msg = {
-    from: "alessiocrea@flashbeing.com", // Use the email address or domain you verified
+    from: "no-reply@flashbeing.com", // Use the email address or domain you verified
     personalizations: [
       {
         to: [
