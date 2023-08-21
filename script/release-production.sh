a94c9686c55cb6633b619df89cc3c363dc58ec76
Simone Testa <sim.testa97@gmail.com>
2023-08-21 14:43:20 +0200

    ci: fix deploy script

--- script/release-production.sh
+++ script/release-production.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-git switch development &&
+git switch main &&
 production_last_local_commit_sha=$(git log -1 --pretty=%H | cat) &&
 git pull &&
 production_last_origin_commit_sha=$(git log -1 --pretty=%H | cat)
 
 if [ "$production_last_local_commit_sha" != "$production_last_origin_commit_sha" ]; then 
-  echo  -e "\033[33mwarning\033[0m your local development commit is different from remote one"
+  echo  -e "\033[33mwarning\033[0m your local main commit is different from remote one"
   echo "Check the changes!"
   exit 1
 fi
